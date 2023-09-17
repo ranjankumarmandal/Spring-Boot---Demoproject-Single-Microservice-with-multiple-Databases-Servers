cfaa5adc9d0a794f3d04fd3ef3bc0aee1729f132
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-17 17:24:21 +0530

    Spring Boot: completed workig for entity (table) Person for Person DB

---
+++ src/main/java/com/example/singlemicroservicewithmultidbservers/personDb/Person.java
@@ -0,0 +1,21 @@
+package com.example.singlemicroservicewithmultidbservers.personDb;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+}
