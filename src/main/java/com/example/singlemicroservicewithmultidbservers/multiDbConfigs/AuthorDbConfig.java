a97c8fe496e80367f8fb2e24819221d0c2b8fc97
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-09-17 17:16:08 +0530

    Spring Boot: added last bean for both DBs config file to handle transaction manager

--- src/main/java/com/example/singlemicroservicewithmultidbservers/multiDbConfigs/AuthorDbConfig.java
+++ src/main/java/com/example/singlemicroservicewithmultidbservers/multiDbConfigs/AuthorDbConfig.java
@@ -5,8 +5,10 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -38,5 +40,12 @@ public class AuthorDbConfig {
         em.setJpaVendorAdapter(hibernateJpaVendorAdapter);
         em.setJpaPropertyMap(properties);
         return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager getAuthorTxnManager(){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(getAuthorEM().getObject());
+        return jpaTransactionManager;
     }
 }
--- src/main/java/com/example/singlemicroservicewithmultidbservers/multiDbConfigs/PersonDbConfig.java
+++ src/main/java/com/example/singlemicroservicewithmultidbservers/multiDbConfigs/PersonDbConfig.java
@@ -5,8 +5,10 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -37,5 +39,12 @@ public class PersonDbConfig {
         em.setJpaVendorAdapter(hibernateJpaVendorAdapter);
         em.setJpaPropertyMap(properties);
         return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager getPersonTxnManager(){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(getPersonEM().getObject());
+        return jpaTransactionManager;
     }
 }
